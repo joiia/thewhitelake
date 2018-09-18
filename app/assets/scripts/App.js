0451fe4ebad18e7998c0c3f515602dd1eb1b4e90
Tonya <a.s.bredeleva@gmail.com>
2018-09-18 16:47:49 +0300

    main-page

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,15 +2,17 @@ import $ from 'jquery';
 
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
-import StickyHeader from "./modules/StickyHeader";
+//import StickyHeader from "./modules/StickyHeader";
 import OpenModal from "./modules/OpenModal";
+import ImageOrientation from "./modules/ImageOrientation";
 
 var mobileMenu = new MobileMenu();
 
 new RevealOnScroll($('.row'), '85%');
 
-var stickyHeader = new StickyHeader();
+//var stickyHeader = new StickyHeader();
 
 var openModal = new OpenModal();  
 
- 
+new ImageOrientation($('.feed-item__img img'));
+
